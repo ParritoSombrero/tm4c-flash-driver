e11b12399f2817e7a801ec0fa4d9b10d16aacb86
ParritoSombrero <150304749+ParritoSombrero@users.noreply.github.com>
2026-07-08 09:39:25 -0500

    Add files via upload

---
+++ src/flash.c
@@ -0,0 +1,39 @@
+#include "flash.h"
+
+// To program a 32-bit word
+// 1. Write source data to the FMD register.
+// 2. Write the target address to the FMA register.
+// 3. Write the Flash memory write key and the WRITE bit to the FMC register. Depending on the
+// value of the KEY bit in the BOOTCFG register, the value 0xA442 or 0x71D5 must be written
+// into the WRKEY field for a Flash memory write to occur.
+// 4. Poll the FMC register until the WRITE bit is cleared.
+
+// To perform an erase of a 1-KB page
+// 1. Write the page address to the FMA register.
+// 2. Write the Flash memory write key and the ERASE bit to the FMC register. Depending on the
+// value of the KEY bit in the BOOTCFG register, the value 0xA442 or 0x71D5 must be written
+// into the WRKEY field for a Flash memory write to occur.
+// 3. Poll the FMC register until the ERASE bit is cleared o
+
+void flashAddress(uint32_t address, uint32_t data) {
+    FLASH_FMD = data;
+    FLASH_FMA = address;
+    FLASH_FMC = (0xA442 << 16) | 0x1;
+
+    while (FLASH_FMC & 0x01) {
+        
+    }
+}
+
+void eraseAddress(uint32_t address) {
+    FLASH_FMA = address;
+    FLASH_FMC = (0xA442 << 16) | 0x2;
+
+    while (FLASH_FMC & 0x02) {
+        
+    }
+}
+
+bool verifyAddress(uint32_t address, uint32_t data) {
+    return (*(volatile uint32_t *)address == data);
+}
---
+++ src/flash.h
@@ -0,0 +1,25 @@
+#ifndef FLASH_H
+#define FLASH_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// To program a 32-bit word
+// 1. Write source data to the FMD register.
+// 2. Write the target address to the FMA register.
+// 3. Write the Flash memory write key and the WRITE bit to the FMC register. Depending on the
+// value of the KEY bit in the BOOTCFG register, the value 0xA442 or 0x71D5 must be written
+// into the WRKEY field for a Flash memory write to occur.
+// 4. Poll the FMC register until the WRITE bit is cleared.
+
+
+#define FLASH_BASE 0x400FD000
+#define FLASH_FMD (*((volatile uint32_t*)(FLASH_BASE + 0x004)))
+#define FLASH_FMA (*((volatile uint32_t*)(FLASH_BASE + 0x000)))
+#define FLASH_FMC (*((volatile uint32_t*)(FLASH_BASE + 0x008)))
+
+void flashAddress(uint32_t address, uint32_t data);
+bool verifyAddress (uint32_t address, uint32_t data);
+void eraseAddress(uint32_t address);
+
+#endif 
